9eaddab6bd0d553ff5ce0b94507446c2c4b79697
Martin Bertsche <martin72216@googlemail.com>
2018-07-08 20:11:54 +0200

    fixed project naming

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='deferred',
+	name='deferred-mb',
 	version='0.0.1',
 	url='https://github.com/everclear72216/deferred',
-	description='A Javascript style promise with notifications',
+	description='A Javascript-style defer() implementation for promises in multi-threaded Python applications.',
 	author='Martin Bertsche',
 	author_email='martin72216@googlemail.com',
 	license='MIT',
