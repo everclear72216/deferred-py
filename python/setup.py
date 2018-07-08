59f432dbcad3aba172d4837f460578c38ebc2aec
Martin Bertsche <martin72216@googlemail.com>
2018-07-08 08:42:41 +0200

    added setup call

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name='deferred',
+	version='0.0.1',
+	url='https://github.com/everclear72216/deferred',
+	description='A Javascript style promise with notifications',
+	author='Martin Bertsche',
+	author_email='martin72216@googlemail.com',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 4 - Beta'
+	],
+	packages=find_packages()
+)
